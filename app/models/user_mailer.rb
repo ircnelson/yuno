0cca94e2fa302cd1e0ba67ac95f0a1b38e225871
Nelson Corrêa <ircnelson@gmail.com>
2009-02-04 12:31:32 -0300

    Configurado UserMailer

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,23 +1,23 @@
 class UserMailer < ActionMailer::Base
+	include Translate
   def signup_notification(user)
     setup_email(user)
-    @subject    += 'Please activate your new account'
-  
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @subject    += t('authentication.successfull.signup_notification')
+    @body[:url]  = "http://www.yuno.com.br/#{t('routes.actions.activate')}/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
-    @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @subject    += t('authentication.successfull.activation')
+    @body[:url]  = "http://www.yuno.com.br"
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "admin@yuno.com.br"
+      @subject     = "[www.yuno.com.br] "
       @sent_on     = Time.now
       @body[:user] = user
     end
